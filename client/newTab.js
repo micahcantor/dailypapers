71b2afec9dfb79529f7932abb1a5766a31ffa0ef
Micah Cantor <micahcantor01@gmail.com>
2020-07-02 00:14:35 -0400

    pull imgur url from api

--- client/newTab.js
+++ client/newTab.js
@@ -1,13 +1,19 @@
 (async function pullDetails() {
+    console.log("waiting for heroku")
     const url = "https://reddit-chrome-wallpapers.herokuapp.com/details";
     const response = await fetch(url);
     const data = await response.json();
+    console.log("response received " + data.ImageURL)
     chrome.storage.sync.set(data);
 }())
 
 chrome.storage.sync.get(null, (items) => {
-    if (items.Author != null && items.Permalink != null) {
-        document.getElementById("post_link").setAttribute("href", "https://reddit.com" + items.Permalink);
+    console.log(items)
+    if (items.Author != null && items.Permalink != null && items.ImageURL != null) {
+        const submittedBox = document.getElementById("post_link")
+        submittedBox.setAttribute("href", "https://reddit.com" + items.Permalink);
+        submittedBox.style.visibility = "visible";
         document.getElementById("author").innerText = "Submitted by u/" + items.Author;
+        document.getElementById("body").style.backgroundImage = "url(" + items.ImageURL + ")";
     }
 })
